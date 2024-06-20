94e4c8693603aa42815c8a7c538bcdf35e63d709
Vinicius <vini383@gmail.com>
2024-06-20 12:26:30 -0300

    feat: add testing config

---
+++ Back.Interview.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,30 @@
+﻿
+
+using Back.Interview.Domain.Repositories;
+using Back.Interview.Repository.Repositories.DataInterview;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Back.Interview.Tests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Remova o repositório existente
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(IDataInterviewRepository));
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                // Adicione um repositório mockado
+                services.AddScoped<IDataInterviewRepository, DataInterviewRepository>();
+            });
+        }
+    }
+}
---
+++ Back.Interview.Tests/InterviewControllerTestes.cs
@@ -0,0 +1,23 @@
+﻿namespace Back.Interview.Tests;
+
+public class InterviewControllerTests : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly HttpClient _client;
+
+    public InterviewControllerTests(CustomWebApplicationFactory factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task GetInterviewAsync_ReturnsExpectedMessage()
+    {
+        var expectedResponse = "1 - Teste - Ativo";
+        
+        var response = await _client.GetAsync("/Interview");
+        
+        response.EnsureSuccessStatusCode();
+        var actualResponse = await response.Content.ReadAsStringAsync();
+        Assert.Equal(expectedResponse, actualResponse);
+    }
+}

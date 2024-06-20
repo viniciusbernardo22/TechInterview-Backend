21fd17006f0058efa50a5a9b2c83febf7dda0af0
Vinicius <vini383@gmail.com>
2024-06-20 11:28:44 -0300

    feat: add DataInterviewRepository.cs

---
+++ Back.Interview.Repository/Repositories/DataInterview/DataInterviewRepository.cs
@@ -0,0 +1,9 @@
+﻿using Back.Interview.Domain.Repositories;
+
+namespace Back.Interview.Repository.Repositories.DataInterview;
+
+public class DataInterviewRepository : IDataInterviewRepository
+{
+    public async Task<string> GetDataAsync()
+        => "1 - Teste - Ativo";
+}

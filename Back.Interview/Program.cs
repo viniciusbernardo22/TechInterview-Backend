e58d9ca81e3cd2cbcd1231a0eb0accfa50cacf6f
Vinicius <vini383@gmail.com>
2024-06-20 11:30:17 -0300

    feat: registered DataInterviewRepository

--- Back.Interview/Program.cs
+++ Back.Interview/Program.cs
@@ -1,8 +1,11 @@
+using Back.Interview.Domain.Repositories;
+using Back.Interview.Repository.Repositories.DataInterview;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddScoped<IDataInterviewRepository, DataInterviewRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
